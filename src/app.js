e5f580f570f8842efb93d2d7c3c2ab3451307588
josemaria1988 <idontknow1988@gmail.com>
2023-04-09 16:15:21 -0300

    saving por las dudaings

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import messagesRouter from "./routes/messages.router.js";
 import viewsRouter from "./routes/views.router.js";
 import handlebars from "express-handlebars";
 import __dirname from "./utils.js";
+import mongoose from "mongoose";
  
 const app = express();
 
@@ -16,6 +17,10 @@ app.set("view engine", "handlebars");
 app.listen(8080, () => {
     console.log("Listening on port 8080");
 })
+
+mongoose.connect(
+    "mongodb+srv://josemariasosa:i1MADDwVlOZS2kt6@jmscluster0.gtmsfjl.mongodb.net/?retryWrites=true&w=majority"
+)
 
 app.use("/api/users", usersRouter);
 app.use("/api/messages", messagesRouter);
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -12,6 +12,42 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+    try {
+        const {first_name, last_name, email} = req.body;
+        if(!first_name || !last_name || !email) {
+            return res.status(400).send({status: "error", error: "missing properties"});
+        }
+
+        const user = {
+            first_name,
+            last_name,
+            email,
+        };
+
+        const userCreated = await userModel.create(user)
+
+        return res.send({status: "Success", payload: userCreated})
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+router.put("/:uid", async (req, res) => {
+    try {
+        const {uid} = req.params;
+        const userUpdated = req.body;
+
+        if(!userUpdated) {
+            return res.status(400).send({status: "error", erro: "missing information"});
+        }
+
+       const updatedMongoUser = await  userModel.updateOne({_id: uid}, userUpdated);
+
+       return res.send({status: "Success", payload: updatedMongoUser})
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 export default router;
